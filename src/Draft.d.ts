7f3148b09cd2747abfdcb91474a917492e0a9903
nonamie <53796817+mathfox@users.noreply.github.com>
2024-09-12 05:46:29 +0300

    do not inline the None symbol

--- src/Draft.d.ts
+++ src/Draft.d.ts
@@ -1,13 +1,17 @@
+import type None from "./None";
+
 /**
  * A type that recursively makes the structure writable.
  *
  * This type does not really represent the actual `Draft` class type from Luau.
  */
-type Draft<TInput> = TInput extends ReadonlyMap<infer TKey, infer TValue>
-	? Map<Draft<TKey>, Draft<TValue>>
-	: TInput extends ReadonlySet<infer TValue>
-		? Set<Draft<TValue>>
-		: { -readonly [TKey in keyof TInput]: Draft<TInput[TKey]> };
+type Draft<TInput> = TInput extends typeof None
+	? TInput
+	: TInput extends ReadonlyMap<infer TKey, infer TValue>
+		? Map<Draft<TKey>, Draft<TValue>>
+		: TInput extends ReadonlySet<infer TValue>
+			? Set<Draft<TValue>>
+			: { -readonly [TKey in keyof TInput]: Draft<TInput[TKey]> };
 
 declare const Draft: new <TBase>(base: TBase) => Draft<TBase>;
 
