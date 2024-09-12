a79c0ea74270c3a0327c7ac16f1e1521297f9531
nonamie <53796817+mathfox@users.noreply.github.com>
2024-09-12 06:04:14 +0300

    fix object type mappings

--- src/Draft.d.ts
+++ src/Draft.d.ts
@@ -1,17 +1,26 @@
-import type None from "./None";
+type PrimitiveType = CheckablePrimitives[keyof Omit<CheckablePrimitives, "userdata" | "function" | "table">];
+
+/** Object types that should never be mapped */
+type AtomicObject = Callback | Promise<any>;
 
 /**
  * A type that recursively makes the structure writable.
  *
  * This type does not really represent the actual `Draft` class type from Luau.
  */
-type Draft<TInput> = TInput extends typeof None
+type Draft<TInput> = TInput extends AtomicObject
 	? TInput
 	: TInput extends ReadonlyMap<infer TKey, infer TValue>
 		? Map<Draft<TKey>, Draft<TValue>>
 		: TInput extends ReadonlySet<infer TValue>
 			? Set<Draft<TValue>>
-			: { -readonly [TKey in keyof TInput]: Draft<TInput[TKey]> };
+			: TInput extends WeakMap<any, any> | WeakSet<any>
+				? TInput
+				: TInput extends PrimitiveType
+					? TInput
+					: TInput extends object
+						? { -readonly [TKey in keyof TInput]: Draft<TInput[TKey]> }
+						: TInput;
 
 declare const Draft: new <TBase>(base: TBase) => Draft<TBase>;
 
--- src/examples/nested-original-usage.ts
+++ src/examples/nested-original-usage.ts
@@ -4,6 +4,9 @@ import produce from "../produce";
 const value = {
 	nested: {
 		valueMap: new ReadonlyMap<string, number>(),
+		callback: () => {
+			return 3;
+		},
 	},
 };
 
@@ -12,5 +15,6 @@ const newValue = produce(value, (draft) => {
 	original(draft);
 
 	draft.nested.valueMap;
+	draft.nested.valueMap.set("test", 3);
 	original(draft.nested.valueMap);
 });
