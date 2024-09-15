cb48f5b339ab751bc01e14d7fe0133e695cb605c
nonamie <53796817+mathfox@users.noreply.github.com>
2024-09-15 05:52:17 +0300

    small tweaks

--- src/index.d.ts
+++ src/index.d.ts
@@ -3,7 +3,7 @@ import makeDraftSafe from "./makeDraftSafe";
 import makeDraftSafeReadOnly from "./makeDraftSafeReadOnly";
 
 import type Draft from "./Draft";
-import table from "./table";
+import table, { clear, concat, find, insert, remove, sort } from "./table";
 
 import None from "./None";
 import finishDraft from "./finishDraft";
@@ -12,8 +12,14 @@ import isDraftable from "./isDraftable";
 import original from "./original";
 import produce from "./produce";
 
-export function createDraft<TBase extends object>(base: TBase): Draft<TBase>;
+declare namespace Immut {
+	export function createDraft<TBase extends object>(base: TBase): Draft<TBase>;
 
-export { finishDraft, finishDraft as current, isDraft, isDraftable, original, produce, table, None, None as nothing };
+	export { finishDraft, finishDraft as current, isDraft, isDraftable, original, produce, table, None, None as nothing };
 
-export { makeDraftSafe, makeDraftSafeReadOnly };
+	export { makeDraftSafe, makeDraftSafeReadOnly };
+
+	export { remove, sort, clear, find, concat, insert };
+}
+
+export = Immut;
---
+++ src/index.test.ts
@@ -0,0 +1,6 @@
+import { expect, it } from "@rbxts/jest-globals";
+import Immut from ".";
+
+it("should load successfully", () => {
+	expect(Immut).toBeTruthy();
+});
